f9d5f45e10dfa37df0251f36322f620ee966cc94
MatveiBG <legomegamat@gmail.com>
2023-08-08 11:43:52 +0200

    implement entry validation and new directory creation

--- image_changer copy.py
+++ image_changer copy.py
@@ -29,13 +29,16 @@ def yesDirectories(dir1: Direc, dir2: Direc):
     if not dir1.isEmpty() and not dir2.isEmpty():
         return True
 
-def transform(base_dir, final_dir, removeBool):
+def transform(base_dir, final_dir, removeBool, finBool):
         ''' Transforms a folder of images in required size,
         removes background if desired.
         base_dir: directory where files are taken from
         final_dir: directory where files are put after transformation
         removeBool: if True, images will go through background removal
         '''
+        if finBool:
+            os.mkdir(f'{base_dir}_new')
+            final_dir = f'{base_dir}_new'
         basewidth = 912
         baseheight = 1100
         print("Starting")
@@ -69,23 +72,31 @@ def transform(base_dir, final_dir, removeBool):
                     print(f"Error with file {images}")
         print("Finished")
 
-def checkButton(base_dir,final_dir):
+def checkButton(base_dir,final_dir, locked):
     ''' Called when one of the choosing buttons is pressed,
     if conditions are valiadated activates transform button,
     else keeps it off'''
-    if yesDirectories(final_dir, base_dir):
-        transButton.configure(state=NORMAL)
-        window.update()
+    if locked: #if the final directory is auto-created
+        if not base_dir.isEmpty():
+            transButton.configure(state=NORMAL)
+            window.update()
+        else:
+            transButton.configure(state=DISABLED)
+            window.update()
     else:
-        transButton.configure(state=DISABLED)
-        window.update()
+        if yesDirectories(final_dir, base_dir):
+            transButton.configure(state=NORMAL)
+            window.update()
+        else:
+            transButton.configure(state=DISABLED)
+            window.update()
 
-def transformation(base_dir, final_dir, window, removeBool):
+def transformation(base_dir, final_dir, window, removeBool, finBool):
     ''' Called with the press of transform button,
     transforms images and updates status'''
     txtStatus.set("Processing images.")
     window.update()
-    transform(base_dir, final_dir, removeBool)
+    transform(base_dir, final_dir, removeBool, finBool)
     txtStatus.set("Finished.")
     window.update()
 
@@ -109,7 +120,7 @@ window=Tk()
 window.title('File transformer')
 window.geometry("500x275+10+10")
 
-window.bind('<ButtonRelease>', lambda x: checkButton(base_dir, final_dir))
+window.bind('<ButtonRelease>', lambda x: checkButton(base_dir, final_dir, finDirVar.get()))
 window.grid_columnconfigure(1, weight=1)
 #################
 
@@ -124,13 +135,20 @@ checkRmv = Checkbutton(
 checkRmv.grid(row=0, column=1)
 
 #checkbox for final directory creation
+def finDirLock(lock):
+    #locks button to choose final directory
+    if lock:
+        finButton.configure(state=DISABLED)
+    else:
+        finButton.configure(state=NORMAL)
+
 finDirVar = BooleanVar()
 checkCrtDir = Checkbutton(
     window,
     text = 'Create a final directory in same folder as original',
-    variable= removeVar,
-    command = lambda: print(removeVar.get())
-)
+    variable= finDirVar,
+    command = lambda: finDirLock(finDirVar.get())
+    )
 checkCrtDir.grid(row= 1, column= 1)
 
 #size frame
@@ -144,25 +162,66 @@ def checkDigit(P):
     else:
         return False
 vcmd = window.register(checkDigit)
-widthEntry = Entry(sizeFrame, validate='all', validatecommand=(vcmd, '%P'))
+
+#width entry
+def delTextWidth(_):
+    if widthEntry.get()=='Enter Desired Width':
+        widthEntry.delete(0, 'end')
+        widthEntry.configure(validatecommand = (vcmd, '%P'))
+def checkTextWidth(_):
+    if not widthEntry.get():
+        widthEntry.configure(validatecommand = ())
+        widthEntry.insert(0, 'Enter Desired Width')
+
+widthEntry = Entry(sizeFrame, validate='all')
+
+widthEntry.insert(0, 'Enter Desired Width')
 widthEntry.grid(row= 0, column=0)
-heightEntry = Entry(sizeFrame, validate='all', validatecommand=(vcmd, '%P'))
+
+widthEntry.bind("<FocusIn>", delTextWidth)
+widthEntry.bind("<FocusOut>", checkTextWidth)
+
+#height entry
+vcmd2 = window.register(checkDigit)
+def delTextHeight(_):
+    if heightEntry.get()=='Enter Desired Height':
+        heightEntry.delete(0, 'end')
+        heightEntry.configure(validatecommand = (vcmd2, '%P'))
+def checkTextHeigth(_):
+    if not heightEntry.get():
+        heightEntry.configure(validatecommand = ())
+        heightEntry.insert(0, 'Enter Desired Height')
+
+heightEntry = Entry(sizeFrame, validate='all')
+
+heightEntry.insert(0, 'Enter Desired Height')
 heightEntry.grid(row= 1, column=0)
 
+heightEntry.bind("<FocusIn>", delTextHeight)
+heightEntry.bind("<FocusOut>", checkTextHeigth)
+
 #size validation/change button
+def lockEntry():
+    widthEntry.configure(state= DISABLED)
+    heightEntry.configure(state= DISABLED)
+
 validateButton=Button(
             sizeFrame,
             text="Validate",
             fg='blue',
-            command= lambda: dirButton(txtFin, final_dir, False)
+            command= lockEntry
             )
 validateButton.grid(row = 0, column = 1)
+
+def unlockEntry():
+    widthEntry.configure(state= NORMAL)
+    heightEntry.configure(state= NORMAL)
 
 changeButton=Button(
             sizeFrame,
             text="Change",
             fg='blue',
-            command= lambda: dirButton(txtFin, final_dir, False)
+            command= unlockEntry
             )
 changeButton.grid(row = 1, column = 1)
 
@@ -212,7 +271,7 @@ transButton=Button(
             window,
             text="Transform",
             fg='blue',
-            command= lambda: transformation(base_dir, final_dir, window, removeVar.get()),
+            command= lambda: transformation(base_dir, final_dir, window, removeVar.get(), finDirVar.get()),
             state= DISABLED         
             )
 transButton.grid(row= 4, column = 1, pady= 0)
