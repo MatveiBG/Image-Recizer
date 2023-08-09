71f4796671ddb18f5fb30e9dc4bc48ef78c1f0b7
MatveiBG <legomegamat@gmail.com>
2023-08-09 10:55:24 +0200

    change file name + optimize some code

--- image_changer_v2.py
+++ image_changer_v2.py
@@ -1,8 +1,9 @@
-import PIL.Image
-import os
-import tkinter
-from tkinter import *
+''' Image Changer app that lets users bulk recize images'''
+
 from tkinter import filedialog
+from tkinter import *
+import os
+import PIL.Image
 from rembg import remove
 
 class Direc():
@@ -11,14 +12,15 @@ class Direc():
         self.chem = ""
 
     def askChem(self):
-        self.chem = direc = filedialog.askdirectory(title='Select a directory')
+        self.chem = filedialog.askdirectory(title='Select a directory')
 
     def __repr__(self):
         return self.chem
-    
+
     def isEmpty(self):
+        ''' Used to check if a directory has been chosen'''
         return self.chem == ""
-    
+
 def noDirectories(dir1: Direc, dir2: Direc):
     ''' Returns true if both directories have not been filled yet'''
     if dir1.isEmpty() and dir2.isEmpty():
@@ -29,75 +31,84 @@ def yesDirectories(dir1: Direc, dir2: Direc):
     if not dir1.isEmpty() and not dir2.isEmpty():
         return True
 
-def transform(base_dir, final_dir, removeBool, finBool, size, back, color):
-        ''' Transforms a folder of images in required size,
-        removes background if desired.
-        base_dir: directory where files are taken from
-        final_dir: directory where files are put after transformation
-        removeBool: if True, images will go through background removal
-        '''
-        if finBool:
-            finStr = f'{base_dir}_new/'
-            print(finStr)
-            if os.path.exists(finStr):
-                counter = 1
-                finStr = finStr[:-1] + f'_({counter})/'
-                while os.path.exists(finStr):
-                    counter += 1
-                    finStr = finStr[:-3] + f'{counter})/'
-            os.mkdir(finStr)
-            final_dir = finStr
-        basewidth = size[0]
-        baseheight = size[1]
-        print(basewidth, baseheight)
-        for images in os.listdir(str(base_dir)):
-            bg = PIL.Image.new(mode="RGBA", size=(back,back), color=None if color =='Transparent' else color)
-            # check if the image ends with compatible extension
-            if (images.endswith(".png") or images.endswith(".jpeg") or images.endswith(".jpg") or images.endswith(".webp")):
-                try:
-                    img = PIL.Image.open(f"{str(base_dir)}/{images}")
-                    img = img.convert("RGBA")
-                    wpercent = (basewidth/float(img.size[0]))
-                    hsize = int((float(img.size[1])*float(wpercent)))
-                    if int(hsize) < baseheight:
-                        img = img.resize((basewidth,hsize), PIL.Image.Resampling.LANCZOS)
+def transform(base, final, removeBool, finBool, size, back, color):
+    ''' Transforms a folder of images in required size,
+    removes background if desired.
+    base_dir: directory where files are taken from
+    final_dir: directory where files are put after transformation
+    removeBool: if True, images will go through background removal
+    '''
+    if finBool:
+        finStr = f'{base}_new/'
+        print(finStr)
+        if os.path.exists(finStr):
+            counter = 1
+            finStr = finStr[:-1] + f'_({counter})/'
+            while os.path.exists(finStr):
+                counter += 1
+                finStr = finStr[:-3] + f'{counter})/'
+        os.mkdir(finStr)
+        final = finStr
+    basewidth = size[0]
+    baseheight = size[1]
+    print(basewidth, baseheight)
+    for images in os.listdir(str(base)):
+        bg = PIL.Image.new(
+            mode="RGBA",
+            size=(back,back),
+            color=None if color =='Transparent' else color
+        )
+        # check if the image ends with compatible extension
+        if (
+            images.endswith(".png")
+            or images.endswith(".jpeg")
+            or images.endswith(".jpg")
+            or images.endswith(".webp")
+            ):
+            try:
+                img = PIL.Image.open(f"{str(base)}/{images}")
+                img = img.convert("RGBA")
+                wpercent = basewidth/float(img.size[0])
+                hsize = int((float(img.size[1])*float(wpercent)))
+                if int(hsize) < baseheight:
+                    img = img.resize((basewidth,hsize), PIL.Image.Resampling.LANCZOS)
+                else:
+                    wpercent = baseheight/float(img.size[1])
+                    hsize = int((float(img.size[0])*float(wpercent)))
+                    img = img.resize((hsize, baseheight), PIL.Image.Resampling.LANCZOS)
+                if removeBool:
+                    img = remove(img)
+                bg.paste(img, (int( back/2 - img.size[0]/2 ), int( back/2 - img.size[1]/2 ) ), img)
+                new_name = ""
+                for lettres in images:
+                    if lettres != '.':
+                        new_name += lettres
                     else:
-                        wpercent = (baseheight/float(img.size[1]))
-                        hsize = int((float(img.size[0])*float(wpercent)))
-                        img = img.resize((hsize, baseheight), PIL.Image.Resampling.LANCZOS)
-                    if removeBool:
-                        img = remove(img)
-                    bg.paste(img, (int( back/2 - img.size[0]/2 ), int( back/2 - img.size[1]/2 ) ), img)
-                    new_name = ""
-                    for lettres in images:
-                        if lettres != '.':
-                            new_name += lettres
-                        else:
-                            break
-                    bg.save(f'{str(final_dir)}/{new_name}_new.png') #final folder
-                    
-                except TypeError:
-                    print(f"Error with file {images}")
+                        break
+                bg.save(f'{str(final)}/{new_name}_new.png') #final folder
 
-def checkButton(base_dir,final_dir, locked):
+            except TypeError:
+                print(f"Error with file {images}")
+
+def checkButton(base, final, locked):
     ''' Called when one of the choosing buttons is pressed,
     if conditions are valiadated activates transform button,
     else keeps it off'''
     if widthEntry["state"] == DISABLED:
         if locked: #if the final directory is auto-created
-            if not base_dir.isEmpty():
+            if not base.isEmpty():
                 transButton.configure(state=NORMAL)
             else:
                 transButton.configure(state=DISABLED)
         else:
-            if yesDirectories(final_dir, base_dir):
+            if yesDirectories(final, base):
                 transButton.configure(state=NORMAL)
             else:
                 transButton.configure(state=DISABLED)
-                
+
     else:
         transButton.configure(state=DISABLED)
-        
+
     if locked:
         txtFin.set('Final directory will be created.')
     else:
@@ -105,7 +116,7 @@ def checkButton(base_dir,final_dir, locked):
 
     window.update()
 
-def transformation(base_dir, final_dir, window, removeBool, finBool, size, bg, color):
+def transformation(base, final, win, removeBool, finBool, size, bg, color):
     ''' Called with the press of transform button,
     transforms images and updates status'''
     newSize = []
@@ -118,23 +129,23 @@ def transformation(base_dir, final_dir, window, removeBool, finBool, size, bg, c
     else:
         newSize.append(1100)
     if bg.isdigit():
-       bg = int(bg)
+        bg = int(bg)
     else:
-        bg = 1200 
+        bg = 1200
     txtStatus.set("Processing images.")
-    window.update()
-    transform(base_dir, final_dir, removeBool, finBool, newSize, bg, color)
+    win.update()
+    transform(base, final, removeBool, finBool, newSize, bg, color)
     txtStatus.set("Finished.")
-    window.update()
+    win.update()
 
-def dirButton(txt, dir, strt):
+def dirButton(txt, direc, strt):
     ''' Called with the press of directory buttons,
     used to get user choice of directory and show selected directory after'''
-    dir.askChem()
-    if dir.isEmpty(): #if no directory has been choosen displays instructions
+    direc.askChem()
+    if direc.isEmpty(): #if no directory has been choosen displays instructions
         txt.set("Choose a starting directory." if strt else  "Choose a final directory.")
     else:
-        txt.set(dir)
+        txt.set(direc)
     window.update()
     statusUpdate()
 
@@ -164,7 +175,7 @@ checkRmv.grid(row=0, column=1)
 
 #checkbox for final directory creation
 def finDirLock(lock):
-    #locks button to choose final directory
+    ''' locks button to choose final directory'''
     if lock:
         finButton.configure(state=DISABLED)
     else:
@@ -217,18 +228,19 @@ sizeFrame.grid(row= 3, column=1)
 
 #size entry
 def checkDigit(P):
-    if str.isdigit(P) or P == '':
-        return True
-    else:
-        return False
+    ''' validation command used to only allow numbrs in entry widget'''
+    return str.isdigit(P) or P == ''
+
 vcmd = window.register(checkDigit)
 
 #width entry
 def delTextWidth(_):
+    ''' used to delete text filler in entrey widget'''
     if widthEntry.get()=='Enter Width (912)':
         widthEntry.delete(0, 'end')
         widthEntry.configure(validatecommand = (vcmd, '%P'))
 def checkTextWidth(_):
+    ''' used to put back text filler in entry widget'''
     if not widthEntry.get():
         widthEntry.configure(validatecommand = ())
         widthEntry.insert(0, 'Enter Width (912)')
@@ -244,10 +256,12 @@ widthEntry.bind("<FocusOut>", checkTextWidth)
 #height entry
 vcmd2 = window.register(checkDigit)
 def delTextHeight(_):
+    ''' used to delete text filler in entrey widget'''
     if heightEntry.get()=='Enter Height (1100)':
         heightEntry.delete(0, 'end')
         heightEntry.configure(validatecommand = (vcmd2, '%P'))
 def checkTextHeigth(_):
+    ''' used to put back text filler in entry widget'''
     if not heightEntry.get():
         heightEntry.configure(validatecommand = ())
         heightEntry.insert(0, 'Enter Height (1100)')
@@ -262,10 +276,12 @@ heightEntry.bind("<FocusOut>", checkTextHeigth)
 
 #bg size entry
 def delTextSize(_):
+    ''' used to delete text filler in entrey widget'''
     if sizeEntry.get()=='Enter Background size (1200)':
         sizeEntry.delete(0, 'end')
         sizeEntry.configure(validatecommand = (vcmd, '%P'))
 def checkTextSize(_):
+    ''' used to put back text filler in entry widget'''
     if not sizeEntry.get():
         sizeEntry.configure(validatecommand = ())
         sizeEntry.insert(0, 'Enter Background size (1200)')
@@ -280,6 +296,7 @@ sizeEntry.bind("<FocusOut>", checkTextSize)
 
 #size validation/change button
 def lockEntry():
+    ''' locks entry widgets when validated'''
     widthEntry.configure(state= DISABLED)
     heightEntry.configure(state= DISABLED)
     sizeEntry.configure(state= DISABLED)
@@ -294,6 +311,7 @@ validateButton=Button(
 validateButton.grid(row = 0, column = 1)
 
 def unlockEntry():
+    ''' unlocks entry widgets when change is pressed'''
     widthEntry.configure(state= NORMAL)
     heightEntry.configure(state= NORMAL)
     sizeEntry.configure(state= NORMAL)
@@ -373,6 +391,7 @@ status = Label(
     textvariable= txtStatus
 )
 def statusUpdate():
+    ''' updates status dynamically depending on what is missing to start process'''
     if finDirVar.get():
         if base_dir.isEmpty():
             directories = "choose a directory"
